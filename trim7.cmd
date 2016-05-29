3181368d2193eb33d9ff0a5faab24af12d0822d4
Petar Benke <petar@home>
2016-05-29 13:39:51 +0200

    Initial verison

---
+++ trim7.cmd
@@ -0,0 +1,77 @@
+@echo off
+echo Uninstalling KB971033
+start /w wusa.exe /uninstall /kb:971033 /quiet /norestart
+echo Uninstalling KB2882822
+start /w wusa.exe /uninstall /kb:2882822 /quiet /norestart
+echo Uninstalling KB2902907
+start /w wusa.exe /uninstall /kb:2902907 /quiet /norestart
+echo Uninstalling KB2952664
+start /w wusa.exe /uninstall /kb:2952664 /quiet /norestart
+echo Uninstalling KB2977759
+start /w wusa.exe /uninstall /kb:2977759 /quiet /norestart
+echo Uninstalling KB2990214
+start /w wusa.exe /uninstall /kb:2990214 /quiet /norestart
+echo Uninstalling KB3012973
+start /w wusa.exe /uninstall /kb:3012973 /quiet /norestart
+echo Uninstalling KB3014460
+start /w wusa.exe /uninstall /kb:3014460 /quiet /norestart
+echo Uninstalling KB3015249
+start /w wusa.exe /uninstall /kb:3015249 /quiet /norestart
+echo Uninstalling KB3021917
+start /w wusa.exe /uninstall /kb:3021917 /quiet /norestart
+echo Uninstalling KB3022345
+start /w wusa.exe /uninstall /kb:3022345 /quiet /norestart
+echo Uninstalling KB3035583
+start /w wusa.exe /uninstall /kb:3035583 /quiet /norestart
+echo Uninstalling KB3050265
+start /w wusa.exe /uninstall /kb:3050265 /quiet /norestart
+echo Uninstalling KB3065987
+start /w wusa.exe /uninstall /kb:3065987 /quiet /norestart
+echo Uninstalling KB3068708
+start /w wusa.exe /uninstall /kb:3068708 /quiet /norestart
+echo Uninstalling KB3074677
+start /w wusa.exe /uninstall /kb:3074677 /quiet /norestart
+echo Uninstalling KB3075249
+start /w wusa.exe /uninstall /kb:3075249 /quiet /norestart
+echo Uninstalling KB3075851
+start /w wusa.exe /uninstall /kb:3075851 /quiet /norestart
+echo Uninstalling KB3080149
+start /w wusa.exe /uninstall /kb:3080149 /quiet /norestart
+echo Uninstalling KB3081437
+start /w wusa.exe /uninstall /kb:3081437 /quiet /norestart
+echo Uninstalling KB3081454
+start /w wusa.exe /uninstall /kb:3081454 /quiet /norestart
+echo Uninstalling KB3081954
+start /w wusa.exe /uninstall /kb:3081954 /quiet /norestart
+echo Uninstalling KB3083324
+start /w wusa.exe /uninstall /kb:3083324 /quiet /norestart
+echo Uninstalling KB3083710
+start /w wusa.exe /uninstall /kb:3083710 /quiet /norestart
+echo Uninstalling KB3088195
+start /w wusa.exe /uninstall /kb:3088195 /quiet /norestart
+echo Uninstalling KB3090045
+start /w wusa.exe /uninstall /kb:3090045 /quiet /norestart
+echo Uninstalling KB3093983
+start /w wusa.exe /uninstall /kb:3093983 /quiet /norestart
+echo Uninstalling KB3102810
+start /w wusa.exe /uninstall /kb:3102810 /quiet /norestart
+echo Uninstalling KB3102812
+start /w wusa.exe /uninstall /kb:3102812 /quiet /norestart
+echo Uninstalling KB3112343
+start /w wusa.exe /uninstall /kb:3112343 /quiet /norestart
+echo Uninstalling KB3118401
+start /w wusa.exe /uninstall /kb:3118401 /quiet /norestart
+echo Uninstalling KB3123862
+start /w wusa.exe /uninstall /kb:3123862 /quiet /norestart
+echo Uninstalling KB3133977
+start /w wusa.exe /uninstall /kb:3133977 /quiet /norestart
+echo Uninstalling KB3135445
+start /w wusa.exe /uninstall /kb:3135445 /quiet /norestart
+echo Uninstalling KB3138612
+start /w wusa.exe /uninstall /kb:3138612 /quiet /norestart
+echo Uninstalling KB3139929
+start /w wusa.exe /uninstall /kb:3139929 /quiet /norestart
+echo Uninstalling KB3146449
+start /w wusa.exe /uninstall /kb:3146449 /quiet /norestart
+echo Uninstalling KB3150513
+start /w wusa.exe /uninstall /kb:3150513 /quiet /norestart
