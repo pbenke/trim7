e419f25f14e940adffc4a78d9cc66bebc4625ef5
Petar Benke <petar@home>
2017-02-11 12:10:38 +0100

    Updated list of scheduled tasks

--- schtasks.bat
+++ schtasks.bat
@@ -1,14 +1,35 @@
 schtasks /change /tn "Microsoft\Windows\Application Experience\AitAgent" /disable >nul
 schtasks /change /tn "Microsoft\Windows\Application Experience\ProgramDataUpdater" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Autochk\Proxy" /disable >nul
 schtasks /change /tn "Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /disable >nul
 schtasks /change /tn "Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /disable >nul
 schtasks /change /tn "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /disable >nul
 schtasks /change /tn "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /disable >nul
-schtasks /change /tn "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticResolver" /disable >nul
-schtasks /change /tn "Microsoft\Windows Defender\MP Scheduled Scan" /disable >nul
-schtasks /change /tn "Microsoft\Windows\Autochk\Proxy" /disable >nul
 schtasks /change /tn "Microsoft\Windows\Defrag\ScheduledDefrag" /disable >nul
 schtasks /change /tn "Microsoft\Windows\Location\Notifications" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Maintenance\WinSAT" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\ActivateWindowsSearch" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\ConfigureInternetTimeService" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\DispatchRecoveryTasks" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\ehDRMInit" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\InstallPlayReady" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\mcupdate" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\MediaCenterRecoveryTask" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\ObjectStoreRecoveryTask" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\OCURActivate" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\OCURDiscovery" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\PBDADiscovery" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\PBDADiscoveryW1" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\PBDADiscoveryW2" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\PeriodicScanRetry" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\PvrRecoveryTask" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\PvrScheduleTask" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\RecordingRestart" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\RegisterSearch" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\ReindexSearchRoot" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\SqlLiteRecoveryTask" /disable >nul
+schtasks /change /tn "Microsoft\Windows\Media Center\UpdateRecordPath" /disable >nul
 schtasks /change /tn "Microsoft\Windows\RemoteAssistance\RemoteAssistanceTask" /disable >nul
 schtasks /change /tn "Microsoft\Windows\Windows Error Reporting\QueueReporting" /disable >nul
 schtasks /change /tn "Microsoft\Windows\WindowsBackup\ConfigNotification" /disable >nul
+schtasks /change /tn "Microsoft\Windows Defender\MP Scheduled Scan" /disable >nul
